aa516ebba9de60a55084872854a97d0922e0ff64
DaniZakaria63 <daniootsutsuki@gmail.com>
2023-01-23 11:25:58 +0700

    setting tablayout

---
+++ app/src/main/java/com/shapeide/rasadesa/adapters/TablayoutAdapter.kt
@@ -0,0 +1,24 @@
+package com.shapeide.rasadesa.adapters
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.shapeide.rasadesa.fragments.DiscoverFragment
+import com.shapeide.rasadesa.fragments.FavouriteFragment
+import com.shapeide.rasadesa.fragments.HomeFragment
+
+internal class TablayoutAdapter(fragment: FragmentActivity) : FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int= 3
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position){
+            0-> { HomeFragment() }
+            1-> { DiscoverFragment() }
+            2-> {FavouriteFragment() }
+            else -> createFragment(0)
+        }
+    }
+}
