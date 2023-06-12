36ae3562a037f0e835ccfc6756194a5aa0058a34
danizakaria63 <daniootsutsuki@gmail.com>
2023-06-13 05:43:57 +0700

    deleting something weird

--- app/src/main/java/com/shapeide/rasadesa/networks/ResponseCategory.kt
+++
@@ -1,3 +0,0 @@
-package com.shapeide.rasadesa.networks
-
-data class ResponseCategory<T>(val categories: List<T>)
--- app/src/main/java/com/shapeide/rasadesa/networks/ResponseMeals.kt
+++
@@ -1,3 +0,0 @@
-package com.shapeide.rasadesa.networks
-
-data class ResponseMeals<T>(val meals: List<T>)
